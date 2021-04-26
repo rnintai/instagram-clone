bbd012ed97612d108b2a3f0b7622316a7b937088
rnintai <ho46825@gmail.com>
2021-04-27 00:26:01 +0900

    0427 00:25 initial commit

---
+++ src/components/Common/NavBar.vue
@@ -0,0 +1,160 @@
+<template>
+  <nav class="NavBar">
+    <div class="NavBar-wrap">
+      <a class="logo">
+        <img
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="메인 로고"
+          class="logo-image"
+        />
+      </a>
+      <!--  -->
+      <div class="search">
+        <!-- <div class="search-placeholder"> -->
+        <input type="text" class="search-input-box" placeholder="검색" />
+        <span class="search-ico">
+          <i class="fas fa-search"></i>
+        </span>
+        <button class="search-clear">
+          <i class="fas fa-times-circle"></i>
+        </button>
+        <!-- </div> -->
+      </div>
+      <!--  -->
+      <ul class="menu">
+        <li class="menu-icon-item">
+          <a href="#none" class="home-link">
+            <i class="fas fa-home"></i>
+          </a>
+        </li>
+        <li class="menu-icon-item">
+          <a href="#none" class="direct-link">
+            <i class="far fa-envelope-open"></i>
+          </a>
+        </li>
+        <li class="menu-icon-item">
+          <a href="#none" class="browse-link">
+            <i class="far fa-compass"></i>
+          </a>
+        </li>
+        <li class="menu-icon-item"></li>
+        <a href="#none" class="liked-link">
+          <i class="far fa-heart"></i>
+        </a>
+        <li class="menu-icon-item">
+          <a href="#none" class="profile">
+            <div class="profile-wrap">
+              <img
+                src="https://user-images.githubusercontent.com/65759076/116090636-0c58dc00-a6df-11eb-8bd0-e74842edadd6.png"
+                alt="프로필"
+                class="profile-img"
+              />
+            </div>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
+
+<style lang="scss" scoped>
+.NavBar {
+  background-color: #fff;
+  border-bottom: 1px solid rgb(219, 219, 219);
+  .NavBar-wrap {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin: 0 auto;
+    width: 70%;
+    height: 54px;
+    .logo {
+      width: 103px;
+      display: inline-block;
+      margin-top: 7px;
+    }
+    .search {
+      display: flex;
+      justify-content: center;
+      cursor: text;
+      align-items: center;
+      position: absolute;
+      left: 50%;
+      width: 200px;
+      height: 25px;
+      transform: translateX(-50%);
+      background-color: var(--bg-grey);
+      border: 1px solid var(--border-grey);
+      border-radius: 3px;
+      color: #8e8e8e;
+      .search-input-box {
+        padding: auto;
+        text-align: center;
+        font-size: 12px;
+        background-color: var(--bg-grey);
+        &:focus {
+          text-align: left;
+          + .search-ico {
+            left: 5%;
+          }
+          ~ .search-clear {
+            display: inline-block;
+          }
+        }
+      }
+      .search-ico {
+        position: absolute;
+        left: 40%;
+        transform: translateX(-50%);
+        margin-bottom: 4px;
+        .fas {
+          font-size: 6px;
+          color: #b9b9b9;
+        }
+      }
+      .search-clear {
+        position: absolute;
+        right: 5%;
+        display: none;
+        .fas {
+          font-size: 14px;
+          color: #b9b9b9;
+        }
+      }
+    }
+
+    .menu {
+      display: flex;
+      align-items: center;
+      padding-right: 20px;
+      .menu-icon-item {
+        margin-left: 20px;
+        width: auto;
+      }
+      .profile {
+        display: inline-block;
+        width: 25px;
+        height: 25px;
+        .profile-wrap {
+          display: inline-block;
+          border-radius: 50%;
+          width: 100%;
+          height: 100%;
+          overflow: hidden;
+          .profile-img {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+}
+@media screen and (max-width: 1000px) {
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <main class="home">i'm home</main>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
+  name: 'Home',
+  components: {},
 };
 </script>
